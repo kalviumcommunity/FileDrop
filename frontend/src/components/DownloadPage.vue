46b21c27d13d090a155fa5539e3b43bf61fbe0eb
Ahamed1846 <116168645+Ahamed1846@users.noreply.github.com>
2023-07-17 10:35:46 +0530

    Added image download functionality
    This commits adds the image download functionality.

--- frontend/src/components/DownloadPage.vue
+++ frontend/src/components/DownloadPage.vue
@@ -5,7 +5,8 @@
                 <div class="text-white text-lg tracking-wider">FILEDROP</div>
             </router-link>
             <div>
-                <router-link to="/upload"><button class="border border-white text-white py-2 px-4 rounded tracking-wide">
+                <router-link to="/upload">
+                    <button class="border border-white text-white py-2 px-4 rounded tracking-wide">
                         Upload
                     </button>
                 </router-link>
@@ -17,20 +18,18 @@
             </div>
             <div class="flex-1 bg-gray-100 flex justify-center items-center">
                 <div v-show="!showFile" class="flex flex-col gap-5 justify-center items-center">
-                    <input type="number" class="border border-black rounded py-2 px-4 no-spinner"
-                        placeholder="Enter the code" ref="inputField" />
-                    <button class="border border-black text-black py-2 px-4 rounded tracking-wide"
-                        @click="showDownloadFile">
+                    <input type="text" class="border border-black rounded py-2 px-4 no-spinner" placeholder="Enter the code"
+                        v-model="inputField" />
+                    <button class="border border-black text-black py-2 px-4 rounded tracking-wide" @click="searchFile">
                         Search
                     </button>
                 </div>
-                <div v-show="showFile">
+                <div v-show="showFile" class="flex flex-col items-center">
                     <div>
-                        <p>Sample folder</p>
-                        <p>4.0 GB</p>
+                        <img :src="getImageUrl()" alt="Sample folder" style="width: 400px; height: auto;" />
                     </div>
                     <br />
-                    <button class="border border-black text-black py-2 px-4 rounded tracking-wide">
+                    <button class="border border-black text-black py-2 px-4 rounded tracking-wide" @click="forceDownload()">
                         Download
                     </button>
                 </div>
@@ -41,19 +40,50 @@
   
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
 
 const showFile = ref(false);
 const inputField = ref('');
 
-const showDownloadFile = () => {
+const searchFile = () => {
     if (inputField.value === '') {
         console.log('no')
         return;
     }
+    console.log(inputField.value);
     showFile.value = true;
 };
-</script>
 
+const getImageUrl = () => {
+    if (showFile.value) {
+        const code = inputField.value;
+        return `https://ucarecdn.com/${code}/`;
+    } else {
+        return '';
+    }
+};
+
+
+const forceDownload = async () => {
+    const imageUrl = getImageUrl();
+    try {
+        const response = await axios.get(imageUrl, { responseType: 'blob' });
+        const blob = new Blob([response.data]);
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'file.png');
+        link.style.display = 'none';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+    } catch (error) {
+        console.error('Error downloading the file:', error);
+    }
+};
+</script>
+  
 <style>
 .no-spinner::-webkit-inner-spin-button,
 .no-spinner::-webkit-outer-spin-button {
